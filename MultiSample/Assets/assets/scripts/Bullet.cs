4b4b27fe81aee3002916653019b62e141252a248
root <root@localhost.localdomain>
2018-04-20 09:12:34 +0000

    tutorial for multi-game

---
+++ MultiSample/Assets/assets/scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour {
+
+    private const int bulletDamage = 10;
+
+    void OnCollisionEnter(Collision collision) {
+        Destroy(gameObject);
+
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null) health.TakeDamage(bulletDamage);
+    }
+}
---
+++ MultiSample/Assets/assets/scripts/Health.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+    private NetworkStartPosition[] spawnPoints;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Dead!");
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+
+            if (spawnPoints != null && spawnPoints.Length > 0)
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+
+            transform.position = spawnPoint;
+        }
+    }
+
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+}
